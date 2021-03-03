85e23d2089e42bc9ab9d23bbc9c3631d0f12768b
JangYongUn <sm970413@naver.com>
2021-03-03 11:44:09 +0900

    ......

--- js/04.product-hooks.js
+++ js/04.product-hooks.js
@@ -1,5 +1,22 @@
 /********** APP **********/
+const App = (props) => {
+	const [query, resultproducts] = useState()
 
+	const commitChange = value => {
+		this.setState({
+			query: value
+			resultProducts: this.state.products.filter(v => v.title.includes(value))
+		})
+	}
+
+	return(
+		<div className="container">
+				<TitleBar query={query} className="my-4" />
+				<Search onChange={this.commitChange} />
+				<Products products={resultProducts} />
+		</div>
+	)
+}
 
 
 /********** TitleBar **********/
